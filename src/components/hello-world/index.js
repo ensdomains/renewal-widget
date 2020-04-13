f42c50b7806bbb1e8f4c1c4eb8cf35dfc51eee86
Makoto <inouemak@googlemail.com>
2020-04-13 19:55:48 +0100

    Add basic widget

--- src/components/hello-world/index.js
+++ src/components/hello-world/index.js
@@ -1,11 +1,24 @@
-import { h, Component } from "preact";
-import "./style.scss";
+import { Component } from "preact";
 
+const styles = {
+  backgroundColor:'white',
+  width:'337px',
+  height:'417px',
+  color:'green',
+  position:'absolute',
+  zIndex: 1111111,
+  bottom: 0,
+  right: 0
+}
+const days = 12
+const num = 8
 export default class App extends Component {
   render(props) {
     return (
-      <div>
-        <h1 style={{ color: props.color }}>Hello, World!</h1>
+      <div style={styles}>
+        <h1>You have {num} ENS names expiring in {days} days </h1>
+        <button>Renew Now</button>
+        <div>Don't show this message again</div>
       </div>
     );
   }
